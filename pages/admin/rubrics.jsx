3fea06ff0ac0069342aefcff5822c552643fd85b
savva-shishak <s.shishak@yandex.ru>
2020-04-16 17:22:20 +0500

    version 1.0.1

---
+++ pages/admin/rubrics.jsx
@@ -0,0 +1,12 @@
+import { ALayout } from "../../components/admin-layout"
+
+const AdminPage = ({}) => {
+
+    return (
+        <ALayout>
+            
+        </ALayout>
+    )
+}
+
+export default AdminPage
